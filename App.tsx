da6b0ff6e1d479f8487cbf960e1c9c19b4da923d
prabathmadushan5@gmail.com <prabathmadushan5@gmail.com>
2021-05-23 17:46:20 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert, Button, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is my firt app</Text>
+      <Button
+        onPress={()=>{
+          Alert.alert(
+            "Alert Title",
+            "My Alert Msg",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") }
+            ]
+          );
+        }}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={()=>{}}
+        value={"welcome"}
+      />
+      <TextInput
+       
+        onChangeText={()=>{}}
+        value={"45"}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +47,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff7b54",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  }
 });
